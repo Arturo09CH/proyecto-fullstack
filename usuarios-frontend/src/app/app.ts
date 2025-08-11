a5864b0de7d65db8e82c422e747742d222423d67
Arturo09CH <arthur30hdezhdez@gmail.com>
2025-08-11 14:27:46 -0600

    Fix configure standalone root component with external template and route support

--- usuarios-frontend/src/app/app.ts
+++ usuarios-frontend/src/app/app.ts
@@ -1,26 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { UsuariosListComponent } from './usuarios-list/usuarios-list.component';
-import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
 
+
+
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HttpClientModule,
-    UsuariosListComponent,
-    UsuarioFormComponent,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('usuarios-frontend');
-    recargarUsuarios() {
-    // Aquí puedes recargar la lista de usuarios o solo dejarlo vacío
-    console.log('Evento usuarioGuardado recibido');
-  }
-}
+export class App {}
